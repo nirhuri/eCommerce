e7b2120d73fc99a669145cbc0f5e408d7516807e
nirhuri <hurinir@gmail.com>
2021-01-25 00:34:42 +0200

    starting cart API, Schema and routes defiened

--- server/src/index.js
+++ server/src/index.js
@@ -10,11 +10,13 @@ const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin/auth');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
+const cartRoutes = require('./routes/cart');
 
 app.use('/api', authRoutes);
 app.use('/api', adminRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
+app.use('/api', cartRoutes);
 
 const port = process.env.PORT;
 app.listen(port, () => {
