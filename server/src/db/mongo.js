bdc263891c3fc32911eea3e45ba7e7f21a610efd
nirhuri <hurinir@gmail.com>
2021-01-26 00:25:05 +0200

    cart product update API in progress

--- server/src/db/mongo.js
+++ server/src/db/mongo.js
@@ -5,6 +5,7 @@ mongoose.connect(
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true
+        useCreateIndex: true,
+        useFindAndModify: false
     }
 )
